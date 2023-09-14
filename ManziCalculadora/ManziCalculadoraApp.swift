40e38ed9bffa381ce9b06e75be668f3d70220b28
Christian Manzaraz <63519258+Manzaraz@users.noreply.github.com>
2023-09-14 19:14:28 -0300

    Initial Commit

---
+++ ManziCalculadora/ManziCalculadoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  ManziCalculadoraApp.swift
+//  ManziCalculadora
+//
+//  Created by Christian Manzaraz on 14/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ManziCalculadoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
